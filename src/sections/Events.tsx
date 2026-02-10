86a81bc75719ffaa54c5feb55d7e91715a67e062
admin <libanahmed772@gmail.com>
2026-02-10 12:37:09 +0000

    fixed hero poster

--- src/sections/Events.tsx
+++ src/sections/Events.tsx
@@ -113,13 +113,13 @@ export function Events() {
     <section id="events">
       {/* FULL SCREEN POSTER GALLERY - TOP SECTION */}
       {allPosters.length > 0 ? (
-        <div className="relative w-full h-screen">
+        <div className="w-full py-20 flex justify-center items-center bg-background">
           {/* Full Screen Poster Image */}
           <div className="absolute inset-0">
             <img
               src={allPosters[currentPosterIndex].poster}
               alt={allPosters[currentPosterIndex].title}
-              className="w-full h-full object-cover"
+              className="w-full h-full object-contain"
             />
           </div>
 
