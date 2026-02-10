c5ff7921a1150d3ddd433c4d1c6323357aca0ca8
admin <libanahmed772@gmail.com>
2026-02-10 11:37:20 +0000

    fix supabase anon key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   "https://ohzvzhfskxamhsluxjxl.supabase.co",
-  "sb_publishable_Ws5OzOOV7UAr9pykkaV5Sg_-8xxNcCv"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9oenZ6aGZza3hhbWhzbHV4anhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2OTIxMzksImV4cCI6MjA4NjI2ODEzOX0.atDYpUEAOs-TCmOEjCCf3LA4hKVEa_ATeVGOtEXWmbs"
 );
